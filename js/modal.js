052380703ffca805bd0d4a86b00b634b0cb353ac
Kalinga <kalingakhatua@gmail.com>
2023-04-28 00:26:01 +0530

    modal

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+console.log("modal");
+const modal = document.querySelector("#modal");
+const loginButtons = document.querySelectorAll(`[data-js="mainButton"]`);
+
+for (const loginButton of loginButtons) {
+	loginButton.addEventListener("click", () => {
+		modal.showModal();
+	});
+}
+window.onclick = function (e) {
+	if (e.target == modal) {
+		modal.close();
+	}
+};
+
+// Modal inside logic
+const signUpGhostButton = document.getElementById("signUp");
+const signInGhostButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpGhostButton.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
+
+signInGhostButton.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
