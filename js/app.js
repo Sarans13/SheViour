c374b555f5e7ae5c7d45b6df87df61feafc9b13e
Sarans13 <114057996+Sarans13@users.noreply.github.com>
2023-04-27 21:23:58 +0530

    initialized firebase

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Intialize app
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7BgkhXxtFzqneJId3jTfBkqT5k-wHbVY",
+  authDomain: "sheviour-681f7.firebaseapp.com",
+  projectId: "sheviour-681f7",
+  storageBucket: "sheviour-681f7.appspot.com",
+  messagingSenderId: "57121534276",
+  appId: "1:57121534276:web:35d1bc344854b63b99a711"
+};
+const firebaseapp = initializeApp(firebaseConfig);
+
