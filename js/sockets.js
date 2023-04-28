e6bbf21cbc288197b0252ffceefeaaae0e2b2d1d
Kalinga <kalingakhatua@gmail.com>
2023-04-28 12:25:13 +0530

    added websockets

---
+++ js/sockets.js
@@ -0,0 +1,17 @@
+const server = io('ws://sheviour.kalingakhatua.repl.co');
+
+server.on('message', text => {
+
+    const el = document.createElement('li');
+    el.innerHTML = text;
+    document.querySelector('.messages-box').appendChild(el)
+
+});
+
+document.querySelector('.messages-btn').onclick = () => {
+
+    const text = document.querySelector('.messages-input').value;
+    console.log("🚀 ~ file: sockets.js:14 ~ document.querySelector ~ text:", text)
+    server.emit('message', text)
+    
+}
--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ const io = require("socket.io")(server, {
 });
 
 io.on("connection", (socket) => {
-	console.log("a user connected");
+	console.log("a user connected", socket.id);
 
-	// socket.on("message", (message) => {
-	// 	console.log(message);
-	// 	io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
-	// });
+	socket.on("message", (message) => {
+		console.log(message);
+		io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
+	});
 });
 
 server.listen(8080, () => console.log("listening on http://localhost:8080"));
